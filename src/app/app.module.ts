1c89e8a638280145946b11f4bc158948e8220bd1
Lakshakumara <lakshakumara@gmail.com>
2024-09-25 22:51:58 +0530

    2024/9/25

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserguideComponent } from './userguide/userguide.component';
+import { NewsComponent } from './news/news.component';
+import { SourcecodeComponent } from './sourcecode/sourcecode.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserguideComponent,
+    NewsComponent,
+    SourcecodeComponent
   ],
   imports: [
     BrowserModule,
